87a17e02233c1742f8c33c85c8d9734fa31fdf71
JessicaFredin <110724442+JessicaFredin@users.noreply.github.com>
2023-04-12 20:37:58 +0200

    Updated

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-let hämtaInAllaParagrafer = document.getElementsByTagName("p");
-let hämtaAllaHtmlElementMedKlassnamnetContent= document.getElementsByClassName("content")
-let 
+let navLink = document.querySelectorAll(".nav-link")
+
+for (let i = 0; i < navLink.length; i++) {
+    navLink[i].addEventListener("click", function () {
+        for (let j = 0; j < navLink.length; j++) {
+            navLink[j].classList.remove("active-link")
+        }
+        navLink[i].classList.add("active-link")
+    })
+}
+
