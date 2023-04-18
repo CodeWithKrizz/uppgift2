3c9abc551db45c89c52c055bb6288f2a8f558776
JessicaFredin <110724442+JessicaFredin@users.noreply.github.com>
2023-04-18 12:27:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -43,21 +43,7 @@ const listItems = [
         "unit": "$"
     }
 ];
-// let shoppingList = {};
-// function initShoppingList() {
-//     for (let listItem of listItems) {
-//         console.log(listItem);
-//         shoppingList[listItem.name] = 0;
-//     }
-// }
-window.onload = function() {    
-    // funktioner anropas här
-    initShoppingList();
-}
-// Slut på JS-koden att utgå från.
 
-// Fråga till gruppen: Ska vi göra vår egen istället? Man måste väl inte ta deras kod eller?
-// Normal for-loop
 let shoppingList = {};
 let cardTitle = document.querySelectorAll(".card-title");
 let cardDescription = document.querySelectorAll(".card-description")
@@ -72,9 +58,12 @@ function initShoppingList() {
         console.log(cardTitle[i])
     }
 }
-// for (let i = 0; i < cardTitle.length; i++) {
-//     cardTitle[i].innerHTML = 
-// }
+
+window.onload = function() {    
+    // funktioner anropas här
+    initShoppingList();
+}
+// Slut på JS-koden att utgå från.
 
 
 // Detta skapar en variabel med alla navigations länkar och loopar igenom dem. För varje klick på en länk, så ska en funktion kallas på som utför koden: att loopa igenom alla element och ta bort klassnamnet .active-link för varje gång en navlänk klickas på och sen ska samma klass läggas till på just den länken som det klickades på. På så sätt ser vi till så att bara ett element kan ha klassen .active-link åt gången. // Jessica
